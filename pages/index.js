2eb55ec58a076fd25576ce8016cc867db91fba26
fantadrinker <yizhi.tsui@gmail.com>
2023-04-23 21:18:51 -0700

    step back

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Fred Cui, ex-Microsoft full stack developer</p>
+        <p>Fred Cui, software developer</p>
         <p>
           <a href="https://nextjs.org/learn">this is where I learned to build this site</a>
         </p>
