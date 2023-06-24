9425a7051bdc9bee60d670d72ef5c298dcb376f8
fantadrinker <yizhi.tsui@gmail.com>
2023-06-23 19:28:02 -0700

    fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/global.css'
+import { Roboto_Slab } from 'next/font/google'
+
+const roboto = Roboto_Slab({
+    weights: [400, 700],
+    subsets: ['latin']
+})
 
 export default function App({ Component, pageProps}) {
-    return <Component {...pageProps} />
+    return <main className={roboto.className}>
+        <Component {...pageProps} />
+    </main>
 }
