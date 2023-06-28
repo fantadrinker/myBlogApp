39602425fcf0e73773170f9b68074d05ee01752c
fantadrinker <yizhi.tsui@gmail.com>
2023-06-28 06:53:36 -0700

    ramda + chevron

--- components/ImageGallery.js
+++ components/ImageGallery.js
@@ -1,14 +1,19 @@
 import Image from "next/image";
+import { add } from "ramda"
 import { useState } from "react";
+import Chevron from "./Chevron";
 
 export default function ImageGallery({ images }) {
   const [activeSlide, setActiveSlide] = useState(0);
   // probably best to use cdn for image urls here
   return (
     <div className="h-96 overflow-hidden flex flex-row justify-center items-center">
-      <div>
-        <button onClick={() => setActiveSlide(activeSlide - 1)} disabled={activeSlide === 0}>Previous</button>
-      </div>
+      <Chevron
+        classNames="mr-2" 
+        direction="left" 
+        onClick={() => setActiveSlide(add(-1))} 
+        disabled={activeSlide === 0}
+      />
       {images.map((image, index) => {
         return (
           <div key={index} className={index === activeSlide? '': 'hidden'}>
@@ -21,9 +26,12 @@ export default function ImageGallery({ images }) {
           </div>
         );
       })}
-      <div>
-        <button onClick={() => setActiveSlide(activeSlide + 1)} disabled={activeSlide === images.length - 1}>Next</button>
-      </div>
+      <Chevron 
+        classNames="ml-2"
+        direction="right" 
+        onClick={() => setActiveSlide(add(1))} 
+        disabled={activeSlide === images.length - 1}
+      />
     </div>
   );
 }
