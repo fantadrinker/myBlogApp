489cf931db42f85f965bed86d43165e50e6e4d6b
fantadrinker <yizhi.tsui@gmail.com>
2023-06-28 06:59:20 -0700

    fix build error

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -29,7 +29,7 @@ export default function Post({postData}) {
       <title>{postData.title}</title>
     </Head>
     <article>
-      <ImageGallery images={postData.images} />
+      {postData.images && (<ImageGallery images={postData.images} />)}
       <h1 data-cy="blog-title" className={utilStyles.headingXl}>{postData.title}</h1>
       <div className={utilStyles.lightText}>
         <Date dateString={postData.date} />
