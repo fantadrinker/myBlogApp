6e8e8f846d2ab2d3575e7e8ad1f564fb6e501416
fantadrinker <yizhi.tsui@gmail.com>
2023-07-25 12:00:12 -0700

    post comment

--- app/api/comment/route.js
+++ app/api/comment/route.js
@@ -1,7 +1,20 @@
+import { postComment } from "../../../lib/database"
 
 export async function POST(request) {
   const { postId, author, comment } = await request.json()
   console.log(111, postId, author, comment)
+  try {
+    const response = await postComment(postId, author, comment)
+    console.log(222, response)
+  } catch (err) {
+    console.log("error posting comment", err)
+    return new Response(JSON.stringify({
+      id: 'none'
+    }), {
+      status: 500,
+    })
+  }
+
   return new Response(JSON.stringify({
     id: 'none'
   }), {
